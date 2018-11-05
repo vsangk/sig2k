267f3f033e4af39cd12d03441188b7cad861b648
Emilio Zand <emilio.zand@sigfig.com>
2018-11-05 14:41:53 -0800

    Remove recentMatches stub Make Profile full screen for the time being Fix Min/Max elo rearrange bio

--- src/Profile.js
+++ src/Profile.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import {Col, Grid, Row} from "react-bootstrap";
-import RecentMatches from "./RecentMatches";
 import './Profile.css';
 
 const BASE_ELO = 1200;
@@ -15,11 +14,10 @@ class Profile extends Component {
     }
 
     render() {
-        console.log(this.props);
         return (
             <Grid>
                 <Row className="profile">
-                    <Col md={6}>
+                    <Col md={12}>
                         <Row className="profile-header">
                             <Col md={4}>
                                 <img className="avatar" src={this.props.userData.avatar} />
@@ -30,6 +28,10 @@ class Profile extends Component {
                             </Col>
                         </Row>
                         <Row className="profile-info">
+                            <Row className="favorite-team-section">
+                                <Col className="favorite-team-title" sm={8}>Favorite Team:</Col>
+                                <Col className="favorite-team" sm={4}>{this.props.userData.favorite_team}</Col>
+                            </Row>
                             <Row className="record-section">
                                 <Col className="record-title" sm={4}>Record:</Col>
                                 <Col className="wins" sm={4}>W{this.props.userData.wins}</Col>
@@ -39,28 +41,21 @@ class Profile extends Component {
                                 <Col className="win-rate-title"sm={8}>Win Percentage:</Col>
                                 <Col className="win-rate" sm={4}>{this.calculateWinRate(this.props.userData.wins, this.props.userData.losses)}</Col>
                             </Row>
+                            <Row className="current-streak-section">
+                                <Col className="current-streak-title" sm={8}>Current Streak:</Col>
+                                <Col className="current-streak" sm={4}>{this.currentStreakText(this.props.userData.current_streak, this.props.userData.current_streak_is_win)}</Col>
+                            </Row>
                             <Row className="streak-section">
                                 <Col className="streak-title" sm={4}>Longest Streak:</Col>
                                 <Col className="win-streak" sm={4}>W{this.props.userData.winning_streak}</Col>
                                 <Col className="lose-streak" sm={4}>L{this.props.userData.losting_streak}</Col>
                             </Row>
-                            <Row className="current-streak-section">
-                                <Col className="current-streak-title" sm={8}>Win Percentage:</Col>
-                                <Col className="current-streak" sm={4}>{this.currentStreakText(this.props.userData.current_streak, this.props.userData.current_streak_is_win)}</Col>
-                            </Row>
                             <Row className="elo-section">
                                 <Col className="elo-history-title" sm={4}>Elo History:</Col>
-                                <Col className="max-elo" sm={4}>Max: {Math.max(this.props.userData.elo_history) + BASE_ELO}</Col>
-                                <Col className="min-elo" sm={4}>Min: {Math.min(this.props.userData.elo_history) + BASE_ELO}</Col>
-                            </Row>
-                            <Row className="favorite-team-section">
-                                <Col className="favorite-team-title" sm={8}>Favorite Team:</Col>
-                                <Col className="favorite-team" sm={4}>{this.props.userData.favorite_team}</Col>
+                                <Col className="max-elo" sm={4}>Max: {Math.max(...this.props.userData.elo_history) + BASE_ELO}</Col>
+                                <Col className="min-elo" sm={4}>Min: {Math.min(...this.props.userData.elo_history) + BASE_ELO}</Col>
                             </Row>
                         </Row>
-                    </Col>
-                    <Col md={6}>
-                        <RecentMatches {...this.props.userMatches}></RecentMatches>
                     </Col>
                 </Row>
             </Grid>
