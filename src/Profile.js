10acc07d4facc12f52d2d423d5794a88c45c4ff5
Emilio Zand <emilio.zand@sigfig.com>
2018-11-07 10:04:12 -0800

    Add Loading Screen to Profile Update Profile Formatting

--- src/Profile.js
+++ src/Profile.js
@@ -17,51 +17,34 @@ class Profile extends Component {
     render() {
         return (
             <Grid>
-                <Row className="profile">
-                    <Col md={6}>
-                        <Row className="profile-header">
-                            <Col md={4}>
-                                <img className="avatar" src={this.props.userData.avatar} />
-                            </Col>
-                            <Col md={8}>
-                                <h1>{this.props.userData.user_name}</h1>
-                                <h2>{BASE_ELO + this.props.userData.elo} ELO</h2>
+                {
+                    Object.keys(this.props.userData).length === 0
+                    ? <div className="center">Loading...</div>
+                    : (
+                        <Row className="profile">
+                            <Col md={12}>
+                                <Row className="profile-header">
+                                    <Col md={4}>
+                                        <img alt="avatar" className="avatar" src={this.props.userData.avatar} />
+                                    </Col>
+                                    <Col md={8}>
+                                        <Row className="tag-line">
+                                            <h1>{this.props.userData.user_name}</h1>
+                                            <h2>{BASE_ELO + this.props.userData.elo} ELO (Max: { Math.max.apply(Math, this.props.userData.elo_history) + BASE_ELO}, Min: { Math.min.apply(Math, this.props.userData.elo_history) + BASE_ELO})</h2>
+                                            <h4>Record: {this.props.userData.wins}-{this.props.userData.losses} ({this.calculateWinRate(this.props.userData.wins, this.props.userData.losses)}%)</h4>
+                                            <h4>Current Streak: {this.currentStreakText(this.props.userData.current_streak, this.props.userData.current_streak_is_win)} (Longest: W{this.props.userData.winning_streak}/L{this.props.userData.losing_streak})</h4>
+                                            <h4>Favorite Team: {this.props.userData.favorite_team}</h4>
+                                        </Row>
+                                    </Col>
+                                </Row>
+                                <Row className="recent-matches">
+                                    <h1>Recent Matches</h1>
+                                    <MatchesTable matchesData={this.props.userMatches} loading={this.props.loading} />
+                                </Row>
                             </Col>
                         </Row>
-                        <Row className="profile-info">
-                            <Row className="favorite-team-section">
-                                <Col className="favorite-team-title" sm={8}>Favorite Team:</Col>
-                                <Col className="favorite-team" sm={4}>{this.props.userData.favorite_team}</Col>
-                            </Row>
-                            <Row className="record-section">
-                                <Col className="record-title" sm={4}>Record:</Col>
-                                <Col className="wins" sm={4}>W{this.props.userData.wins}</Col>
-                                <Col className="losses" sm={4}>L{this.props.userData.losses}</Col>
-                            </Row>
-                            <Row className="win-rate-section">
-                                <Col className="win-rate-title"sm={8}>Win Percentage:</Col>
-                                <Col className="win-rate" sm={4}>{this.calculateWinRate(this.props.userData.wins, this.props.userData.losses)}</Col>
-                            </Row>
-                            <Row className="current-streak-section">
-                                <Col className="current-streak-title" sm={8}>Current Streak:</Col>
-                                <Col className="current-streak" sm={4}>{this.currentStreakText(this.props.userData.current_streak, this.props.userData.current_streak_is_win)}</Col>
-                            </Row>
-                            <Row className="streak-section">
-                                <Col className="streak-title" sm={4}>Longest Streak:</Col>
-                                <Col className="win-streak" sm={4}>W{this.props.userData.winning_streak}</Col>
-                                <Col className="lose-streak" sm={4}>L{this.props.userData.losting_streak}</Col>
-                            </Row>
-                            <Row className="elo-section">
-                                <Col className="elo-history-title" sm={4}>Elo History:</Col>
-                                <Col className="max-elo" sm={4}>Max: { Math.max.apply(Math, this.props.userData.elo_history) + BASE_ELO}</Col>
-                                <Col className="min-elo" sm={4}>Min: { Math.min.apply(Math, this.props.userData.elo_history) + BASE_ELO}</Col>
-                            </Row>
-                        </Row>
-                    </Col>
-                    <Col md={6}>
-                        <MatchesTable matchesData={this.props.userMatches} loading={this.props.loading} />
-                    </Col>
-                </Row>
+                    )
+                }
             </Grid>
         );
     }
